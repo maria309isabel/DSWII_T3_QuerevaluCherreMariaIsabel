8d9e643efc0730fc874c5141f8b51f89eb7e6bff
maria309isabel <isa.mar.xv@gmail.com>
2024-07-02 21:18:05 -0500

    P1 primer commit

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_QuerevaluCherreMariaIsabel/DswiiT3QuerevaluCherreMariaIsabelApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_QuerevaluCherreMariaIsabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3QuerevaluCherreMariaIsabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3QuerevaluCherreMariaIsabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_QuerevaluCherreMariaIsabel/DswiiT3QuerevaluCherreMariaIsabelApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_QuerevaluCherreMariaIsabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3QuerevaluCherreMariaIsabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
